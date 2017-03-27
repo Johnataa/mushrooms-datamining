0430953175cec829c286bac9c3b69f070548b0d2
johnata <johnata05@gmail.com>
2017-03-27 14:36:44 -0400

    Criação do aprendizado de maquina NaiveBayes para analise da base de dados e utilização do app.py

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from sys import path
+path.append('./data')
+path.append('./methods')
+
 from data import Data
 from svm import SVM
+from ann import ANN
+from nb import NaiveBayes
 from time import time
 
 data = Data("mushrooms.csv")
 
+
+print 'ANN Method'
+method = ANN(data)
+i = time()
+method.train()
+method.predict()
+f = time()
+print 'Milisegundos:', f-i
+print 'Acerto:', method.getPercentage()
+
+print '\nSVM Method'
 method = SVM(data)
 i = time()
 method.train()
 method.predict()
 f = time()
-print f-i
-print method.getPercentage()
+print 'Milisegundos:', f-i
+print 'Acerto:', method.getPercentage()
+
+print '\nNaiveBayes Method'
+method = NaiveBayes(data)
+i = time()
+method.train()
+method.predict()
+f = time()
+print 'Milisegundos:', f-i
+print 'Acerto:', method.getPercentage()
